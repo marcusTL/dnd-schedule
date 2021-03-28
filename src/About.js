d2f5502276c9a4b5135b42fd4ce58830c34343be
Problocker <Mikkel@LAPTOP-6PU1S87U>
2021-03-28 11:12:38 +0200

    Tester Footer og noget rute i footeren. Virker PT IKKE.

--- src/About.js
+++ src/About.js
@@ -1,54 +1,9 @@
 import React from 'react'
 
-
-const about = () =>
+const About = () =>
 (
-    <div Class="col-sm-12">
 
-        <div class="container">
-            <div class="jumbotron">
-                <h1>OM OS</h1 >
-                <p>Den Store Leder A/S</p>
+<p>Her er sgu lidt tomt herinde, siden er tom så jeg kan pille med forskellige funktioner uden at ødelægge andre sider, måske.....</p>
 
-            </div>
-        </div>
-
-        <div class="container">
-            <div Class="col-sm-4">
-                <h1>Hvem er vi</h1>
-
-                {/* <h4>
-                    Et gammelt spil spillet med "pen and paper"
-            </h4> */}
-
-                <p>Vi er en gruppe 4.-semesterstuderende på datamatikeruddannelsen på Zealand i Roskilde.</p>
-                <p>Obligatorisk opgave i Single Page WebApps (SPA) React/Angular http://heho-zealand.dk/WebAppsF2020/MandatoryAssignmentDK.html</p>
-                <img src="https://geekandleisure.com/wp-content/uploads/2018/07/Best-DD-Dice-1024x640.jpg" class="img-rounded jumbotronImage img-fluid img-responsive" ></img>
-
-
-            </div>
-
-
-            <div Class="col-sm-4">
-                <h1>Mikkel er sej</h1>
-
-                <p>Mikkel er sej. :) </p>
-            </div>
-
-
-            <div Class="col-sm-4">
-                <h1>Oplysinger</h1>
-
-                <p>Adresse: Zealand</p>
-                <p>Telefon:</p>
-            </div>
-        </div>
-
-        <br></br>
-    
-
-
-    </div>
 )
-
-export default about
+export default About
--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,109 @@
-import React from "react";
-import './footer.css'
+//Mig der bare leger, Bootsrap er lidt scuffed. Work in progress. Løsninger kommer snart. lol
+// import React from 'react';
+// function Footer() {
+//   return (
+//     <div className="main-footer">
+//       <div className="container">
+//         <div className="row">
+//           {/* Kolonne 1 */}
+//           <div className=".col-md-3.col-sm-6">
+//             <h4>TEST!</h4>
+//             <ul className="list-unstyled">
+//               <li>Test, test</li>
+//               <li>Test, test</li>
+//               <li>Test, test</li>
+//               <li>Test, test </li>
+//             </ul>
+//           </div>
 
-const Footer = () => (
-  <div className="footer">
-    <div class="container-fluid" className="footer"></div>
-    <p>Footer... MIKKEL OLSEN ©©©</p>
-  </div>
-);
+//           {/* Kolonne 2 */}
+//           <div className=".col-md-3.col-sm-6">
+//             <h4>TEST!</h4>
+//             <ul className="list-unstyled">
+//               <li>Test, test</li>
+//               <li>Test, test</li>
+//               <li>Test, test</li>
+//               <li>Test, test</li>
+//             </ul>
+//           </div>
+
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// const Footer = () =>
+// (
+//   <footer class="bg-light text-center text-lg-start">
+
+//     <div class="container p-4">
+
+//       <div class="row">
+  
+//         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+//           <h5 class="text-uppercase">Footer Content</h5>
+  
+//           <p>
+//             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+//             molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+//             voluptatem veniam, est atque cumque eum delectus sint!
+//           </p>
+//         </div>
+
+//         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+//           <h5 class="text-uppercase">Links</h5>
+  
+//           <ul class="list-unstyled mb-0">
+//             <li>
+//               <a href="#!" class="text-dark">Link 1</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 2</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 3</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 4</a>
+//             </li>
+//           </ul>
+//         </div>
+
+  
+//         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+//           <h5 class="text-uppercase mb-0">Links</h5>
+  
+//           <ul class="list-unstyled">
+//             <li>
+//               <a href="#!" class="text-dark">Link 1</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 2</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 3</a>
+//             </li>
+//             <li>
+//               <a href="#!" class="text-dark">Link 4</a>
+//             </li>
+//           </ul>
+//         </div>
+
+//       </div>
+ 
+//     </div>
 
 
-export default Footer;
+//     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+//       © 2020 Copyright:
+//       <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+//     </div>
+
+//   </footer>
+
+// )
+
+
+
+
+// export default Footer
