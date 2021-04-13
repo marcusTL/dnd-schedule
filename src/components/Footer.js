5e815451a6699b14d8f20f357ed12665d8bc3fc5
Problocker <Mikkel@LAPTOP-6PU1S87U>
2021-04-13 16:11:29 +0200

    Lille ændring på footer, TEST

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,37 +7,19 @@ function Footer() {
       <div className="footer-middle">
         <div className="container">
           <div className="row">
-            {/* Column 1 */}
+            {/* Kolonne 1 */}
             <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
+              <h4>Nikolaj er sej</h4>
               <ul className="list-unstyled">
-                <li>Lorem ipsum</li>
+                <li>Sejt sej</li>
                 <li>Lorem ipsum</li>
                 <li>Lorem ipsum</li>
                 <li>Lorem ipsum</li>
               </ul>
             </div>
-            {/* Column 2 */}
+            {/* Kolonne 2 */}
             <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-              </ul>
-            </div>
-            {/* Column 3 */}
-            <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
+              <h4>BIF</h4>
               <ul className="list-unstyled">
                 <li>
                   <a href="#">Lorem ipsum</a>
@@ -53,7 +35,25 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            {/* Column 4 */}
+            {/* Kolonne 3 */}
+            <div className="col-md-3 col-sm-6">
+              <h4>Lorem ipsum</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#">Lorem ipsum</a>
+                </li>
+                <li>
+                  <a href="#">Lorem ipsum</a>
+                </li>
+                <li>
+                  <a href="#">Lorem ipsum</a>
+                </li>
+                <li>
+                  <a href="#">Lorem ipsum</a>
+                </li>
+              </ul>
+            </div>
+            {/* Kolonne 4 */}
             <div className="col-md-3 col-sm-6">
               <h4>Lorem ipsum</h4>
               <ul className="list-unstyled">
@@ -72,9 +72,10 @@ function Footer() {
               </ul>
             </div>
           </div>
-          {/* Footer bund */}
+          {/* Footer bund */} 
+          {/* CopyRight-Logo og dato, årstal. */}
           <div className="footer-bottom">
-            <p className="text-xs-center">
+            <p className="text-xs-center"> 
               &copy;{new Date().getFullYear()} Mikkel den smukke - All Rights
               Reserved
             </p>
