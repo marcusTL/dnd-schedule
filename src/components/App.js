4d7171ebbc152e48ff3dad3937aa4fb93a373a89
marcusTL <mtlaursen1@gmail.com>
2021-04-14 10:30:09 +0200

    get is done, maybe searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,11 @@ class App extends Component {
     render() {
       return (
         <div className="App">
-          <Header/>
+          
+         <Header/>
+          
           <Main/>
-          <Footer/>
+          {/* <Footer/> */}
         </div>
       );
     }
